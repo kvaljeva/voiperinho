4558fd522ced98be0549f0eee41699bdaa889dc4
kvaljeva <kvaljeva@foi.hr>
2015-12-04 22:33:27 +0100

    User authorization done

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,4 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/user/{username}/{password}', 'UserController@getUser');
+Route::post('/user', 'UserController@getUser');
