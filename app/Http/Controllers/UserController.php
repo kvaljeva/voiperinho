db24723af3ca5ffb0556312c786c423947759637
kvaljeva <kvaljeva@foi.hr>
2015-12-05 15:34:26 +0100

    Added user object to the response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -113,7 +113,7 @@ class UserController extends Controller
         $user = DB::table('user')->where(['username' => $username, 'password' => $password])->first();
 
         if ($user != null) {
-            echo Response::json(array('status' => 200, 'message' => 'OK'), 200);
+            echo Response::json(array('status' => 200, 'message' => json_encode($user)), 200);
         }
         else {
             echo Response::json(array('status' => 400, 'message' => 'ERR'), 400);
