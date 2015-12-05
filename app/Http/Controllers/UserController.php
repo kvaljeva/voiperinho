fe7fbcec5bc6725122fe641c2a5c97787ece88a3
kvaljeva <kvaljeva@foi.hr>
2015-12-05 16:02:51 +0100

    Object values fix v2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -112,8 +112,8 @@ class UserController extends Controller
         $password = $request->input('password');
         $user = DB::table('user')->where(['username' => $username, 'password' => $password])->first();
 
-        $obj = $user->username;
-        $obj .= $user->password;
+        $obj["username"] = $user->username;
+        $obj["password"] .= $user->emailAddress;
 
         if ($user != null) {
             echo Response::json(array('status' => 200, 'message' => json_encode($obj)), 200);
