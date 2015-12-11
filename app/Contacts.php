bfbf639eb4b9fff6569bee141a8d89425bd50dc8
kvaljeva <kvaljeva@foi.hr>
2015-12-11 01:53:02 +0100

    Fixed delete reference class

--- app/Contacts.php
+++ app/Contacts.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contacts extends Model
 {
-    use SoftDeletes;
+    use SoftDeletingTrait;
 
     protected $table = 'contacts';
     public $timestamps = true;
