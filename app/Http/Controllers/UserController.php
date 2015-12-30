4b91c7e88a197b368e691660f902a7b52025671b
kvaljeva <kvaljeva@foi.hr>
2015-12-30 12:21:45 -0500

    Revorked getting all of the available users query

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -148,6 +148,7 @@ class UserController extends Controller
         $users = User::leftJoin('contacts', 'contacts.contact_id', '=', 'user.id')
             ->where('user.id', '!=', $requesterId)
             ->where('user.username', 'LIKE', '%'.$username.'%')
+	    ->distinct()
             ->get(['user.*']);
 
         if ($users != null)
